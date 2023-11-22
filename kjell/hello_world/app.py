e8e7a5dc11da3cdfcf86c98bba1c1ab60e3d4354
mohyus15 <mohyus15@hotmail.com>
2023-11-22 21:35:40 +0000

    created github actions

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -16,7 +16,7 @@ s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
 # Oppgave 1A
-BUCKET_NAME = "opp1-backed"
+#BUCKET_NAME = "kandidatnr2001"
 
 def lambda_handler(event, context):
 
