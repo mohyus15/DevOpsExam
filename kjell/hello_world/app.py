895583ad61314ccdac8d1c412d6ec6b8b7d6a0aa
mohyus15 <mohyus15@hotmail.com>
2023-11-23 14:27:58 +0000

    created a new brach for testing,no deploy, just building

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -15,7 +15,7 @@ import os
 s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 # Oppgave 1A
-#BUCKET_NAME = "kandidatnr2001"
+#BUCKET_NAME = "kandidatnr2001"#
 
 def lambda_handler(event, context):
 
