4ab5de4dbd97b608b34c08ac5cb4d57a1abff7ad
mohyus15 <mohyus15@hotmail.com>
2023-11-23 13:21:58 +0000

    added lines

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -14,7 +14,6 @@ import os
 
 s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
-
 # Oppgave 1A
 #BUCKET_NAME = "kandidatnr2001"
 
