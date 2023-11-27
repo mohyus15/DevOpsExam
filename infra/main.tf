6e92037192ff1a718ffbdc4c47128cd573d05c0c
mohyus15 <mohyus15@hotmail.com>
2023-11-27 01:59:14 +0000

    add some changes i terraform workflows

--- infra/main.tf
+++ infra/main.tf
@@ -1,30 +1,13 @@
-resource "aws_apprunner_service" "service" {
-  service_name = "2001"
-
-  instance_configuration {
-    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
-  }
-
-  source_configuration {
-    authentication_configuration {
-      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
-    }
-    image_repository {
-      image_configuration {
-        port = "8080"
-      }
-      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
-      image_repository_type = "ECR"
-    }
-    auto_deployments_enabled = true
-  }
+variable "service_name" {
+  description = "Navnet på App Runner-tjenesten"
+  type        = string
+  default     = "2001"  # Standardverdi, kan endres etter behov
 }
 
 resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "2001"
+  name               = var.service_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
-
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -65,9 +48,31 @@ resource "aws_iam_policy" "policy" {
   policy      = data.aws_iam_policy_document.policy.json
 }
 
-
 resource "aws_iam_role_policy_attachment" "attachment" {
   role       = aws_iam_role.role_for_apprunner_service.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
+resource "aws_apprunner_service" "service" {
+  service_name = var.service_name
+
+  instance_configuration {
+    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+    cpu               = 256  # Endret fra standardverdi
+    memory            = 1024 # Endret fra standardverdi
+  }
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+    }
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+      image_identifier      = "244530008913.dkr.ecr.eu-west-1.amazonaws.com/kjell:latest"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = true
+  }
+}
